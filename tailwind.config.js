f21dd6ec77f71bdc4ad420cd01b539a506973258
Ruhul Amin <r_amin105@yahoo.com>
2022-08-17 17:55:02 +0600

    Add open sans font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
@@ -9,15 +8,22 @@ module.exports = {
   darkMode: ['class'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
       fontSize: { tiny: '.625rem' },
+      lineHeight: {
+        12: '3rem',
+        14: '4.25rem',
+      },
       colors: {
         'body-dark': '#2B2B2B',
         'omni-slate-light': '#4C6E86',
         'omni-slate-dark': '#395365',
         primary: '#00BAC0',
         secondary: '#FFB201',
-        'neutral-50': '#fafafa',
-        'neutral-100': '#f5f5f5',
+        //'neutral-50': '#fafafa', //commented out as it is default value
+        //'neutral-100': '#f5f5f5', //commented out as it is default value
       },
       scale: {
         105: '1.05',
